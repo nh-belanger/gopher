791a9acbb54587b3a52433f4d5646bcca28ce036
nh-belanger <nick.h.belanger@gmail.com>
2017-01-30 11:58:50 -0500

    adding teetime controller work

--- app/models/teetime.rb
+++ app/models/teetime.rb
@@ -1,4 +1,4 @@
-class TeeTime < ApplicationRecord
+class Teetime < ApplicationRecord
   validates :date, presence: true, numericality: {only_integer: true,
   validates :time, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2400
   validates :starting_hole, presence: true, numericality: true
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,9 @@ Rails.application.routes.draw do
     resources :teetimes
   end
 
-  resources :teetimes
+
 
 
   resources :members, only: [:index, :destroy]
-  resources :teetimes, only: [:index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
