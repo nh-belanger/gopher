870259f70bc75556e5ab051de987e6d578135091
nh-belanger <nick.h.belanger@gmail.com>
2017-01-29 22:58:27 -0500

    codeship

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,5 @@ class ApplicationController < ActionController::Base
  protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :member_number])
- end
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :member_number
 end
