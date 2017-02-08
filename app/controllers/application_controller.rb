f4790c498a9cfe207a0bac5d27d07229dae04017
nh-belanger <nick.h.belanger@gmail.com>
2017-02-08 03:30:10 -0500

    minor welcome page fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :member_number])
  end
+
+ private
+
+ def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
