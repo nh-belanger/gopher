497d3ad4f40f0698988502a73d089e4b9330b1e4
nh-belanger <nick.h.belanger@gmail.com>
2017-02-08 02:37:15 -0500

    getting heroku to work part 1

--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -1,5 +1,1 @@
 # TODO: Added routes and migrations. probably scrap them. figure out a good way to track user score.
-
-each @scorecard.holes.all do |hole|
-  @scorecard.total_score += hole.score
-end
